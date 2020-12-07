90fadbeb1899e092917afeb8088fcbaa3309c1e0
Joke <jokekalter@gmail.com>
2020-12-07 14:51:11 +0100

    Day 7 1 & 2

--- src/7_1.py
+++ src/7_1.py
@@ -1,18 +1,80 @@
 import fileinput
+import re
+from typing import List
 
 
-def process(input_list: list) -> int:
+def get_outer_bags(bag_requirements: dict, target_bag: str) -> List:
+    return [color for color, sub_bags in bag_requirements.items() if sub_bags.get(target_bag, False)]
+
+
+def loop_exhaustive_outer(bag_requirements: dict, target_bag: str) -> List:
     """
+    Find all direct outer bags for the target bag (shiny_gold in case of day 7_1)
+    Then treat all found outer bags as target bags themselves.
+    Script stops if none of the outer bags have any outer bags themselves.
 
-    :param input_list:
+    :param bag_requirements:
+    :param target_bag:
     :return:
     """
-    total = 0
-    return total
+    final_list = []
+    not_exhausted = True
+    current_list = get_outer_bags(bag_requirements, target_bag)
+    while not_exhausted:
+        final_list += current_list
+        tmp_list = []
+        for bag in current_list:
+            tmp_list += get_outer_bags(bag_requirements, bag)
+        current_list = tmp_list
+        if len(current_list) == 0:
+            not_exhausted = False
+    return final_list
+
+
+def process(input_list: list) -> dict:
+    """
+    Put all bag requirements in a dictionary
+
+    e.g.:
+    'dotted blue bags contain 5 wavy green bags, 3 pale beige bags'
+    if parsed to:
+    'dotted_blue': {
+        'wavy_green': '5',
+        'pale_beige': '3'
+    }
+
+    :param input_list: list of bag requirements
+    :return:
+    """
+    bag_requirements = {}
+    for bag_requirement in input_list:
+        outer_bag = bag_requirement.split(' bags ')[0].replace(' ', '_').lower()
+        bag_requirements[outer_bag] = {}
+
+        sub_bags = bag_requirement.split(' contain ')[1].split(', ')
+        for bag in sub_bags:
+            print(bag)
+            if bag and bag.strip(' ') != "no other bags":
+                bag = bag.lower()
+                parsed_bag = re.findall('([0-9]+) ([a-z]+ [a-z]+) ([a-z]+)', bag)
+                print(parsed_bag)
+                if parsed_bag:
+                    parsed_bag = parsed_bag[0]
+                    n_bags = parsed_bag[0]
+                    bag_color = parsed_bag[1].replace(' ', '_')
+                    print(f'color: {bag_color}, number: {n_bags}')
+                    bag_requirements[outer_bag][bag_color] = n_bags
+            else:
+                print('not parsed')
+    return bag_requirements
 
 
 if __name__ == '__main__':
-    lines = [i.strip('\n') for i in fileinput.input()]
+    lines = [i.strip('\n').rstrip('.') for i in fileinput.input()]
     print(lines[0:10])
     output = process(lines)
     print(f'Output: {output}')
+    outer_bags_all = loop_exhaustive_outer(output, "shiny_gold")
+    print(f'all outers {outer_bags_all}\n'
+          f'set: {set(outer_bags_all)}\n'
+          f'count: {len(set(outer_bags_all))}')
--- src/7_2.py
+++ src/7_2.py
@@ -1,18 +1,89 @@
 import fileinput
+import re
+from typing import List
 
 
-def process(input_list: list) -> int:
+def get_outer_bags(bag_requirements: dict, target_bag: str) -> List:
+    return [color for color, sub_bags in bag_requirements.items() if sub_bags.get(target_bag, False)]
+
+
+def get_inner_bags(bag_requirements: dict, target_bag: str, factor: int = 1):
     """
 
-    :param input_list:
+    :param bag_requirements:
+    :param target_bag:
+    :param factor:
     :return:
     """
-    total = 0
-    return total
+    inner_bags = bag_requirements[target_bag]
+    factored_inner_bags = {key: int(value) * factor for key, value in inner_bags.items()}
+    count = sum([int(n) for color, n in factored_inner_bags.items()])
+    return factored_inner_bags, count
+
+
+def loop_exhaustive_inner(bag_requirements: dict, target_bag: str) -> int:
+    """
+    Find all direct inner bags for the target bag (shiny_gold in case of day 7_2)
+    Multiply the numbers by the parents number and sum all inner bags together.
+    Then treat all found inner bags as target bags themselves and repeat process
+    Script stops if none of the inner bags have any inner bags themselves.
+
+    :param bag_requirements:
+    :param target_bag:
+    :return:
+    """
+    current_dict, total_count = get_inner_bags(bag_requirements, target_bag)
+    not_exhausted = True
+    while not_exhausted:
+        tmp_dict = {}
+        for bag, factor in current_dict.items():
+            l, tmp_count = get_inner_bags(bag_requirements, bag, int(factor))
+            # Take into account that a inner bag can be a inner bag to multiple outer bags
+            # Sum the individual factored inner bag counts when updating the total dictionary
+            tmp_dict = {key: int(tmp_dict.get(key, 0)) + int(l.get(key, 0))
+                        for key in list(l.keys()) + list(tmp_dict.keys())}
+            total_count += tmp_count
+        current_dict = tmp_dict
+        if len(current_dict.keys()) == 0:
+            not_exhausted = False
+    return total_count
+
+
+def process(input_list: list) -> dict:
+    """
+    Put all bag requirements in a dictionary
+
+    e.g.:
+    'dotted blue bags contain 5 wavy green bags, 3 pale beige bags'
+    if parsed to:
+    'dotted_blue': {
+        'wavy_green': '5',
+        'pale_beige': '3'
+    }
+
+    :param input_list: list of bag requirements
+    :return:
+    """
+    bag_requirements = {}
+    for bag_requirement in input_list:
+        outer_bag = bag_requirement.split(' bags ')[0].replace(' ', '_').lower()
+        bag_requirements[outer_bag] = {}
+
+        sub_bags = bag_requirement.split(' contain ')[1].split(', ')
+        for bag in sub_bags:
+            if bag and bag.strip(' ') != "no other bags":
+                bag = bag.lower()
+                parsed_bag = re.findall('([0-9]+) ([a-z]+ [a-z]+) ([a-z]+)', bag)
+                if parsed_bag:
+                    parsed_bag = parsed_bag[0]
+                    n_bags = parsed_bag[0]
+                    bag_color = parsed_bag[1].replace(' ', '_')
+                    bag_requirements[outer_bag][bag_color] = n_bags
+    return bag_requirements
 
 
 if __name__ == '__main__':
-    lines = [i.strip('\n') for i in fileinput.input()]
-    print(lines[0:10])
+    lines = [i.strip('\n').rstrip('.') for i in fileinput.input()]
     output = process(lines)
-    print(f'Output: {output}')
+    inner_bags_count = loop_exhaustive_inner(output, "shiny_gold")
+    print(f'count: {inner_bags_count}')
